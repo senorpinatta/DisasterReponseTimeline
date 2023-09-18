19c7fed4581c39948fb69e9d1f1261bed45e0c2a
senorpinatta <12atrf@queensu.ca>
2023-09-18 12:20:03 +0700

    Update process_data.py
    Added , `if_exists` parameter to the save function to handle the case where the disaster database already exists.

--- data/process_data.py
+++ data/process_data.py
@@ -44,7 +44,7 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql('DisasterData', engine, index=False)
+    df.to_sql('DisasterData', engine, index=False, if_exists = 'replace')
 
 
 def main():
